fda2d625b875a6cd2507d92acda830da82a87183
RomuloRatis <entreratoseleoes@gmail.com>
2022-06-14 21:19:35 -0300

    For and While

---
+++ JS/script.js
@@ -0,0 +1,25 @@
+{
+    for(let numero = 1; numero <= 10; numero++){
+        console.log("for aumentando: ", numero);
+    }
+
+
+    let contagem = 10;
+    while(contagem >= 0){
+        console.log("While diminuindo: ", contagem);
+        contagem --;
+
+    }
+
+    for(let number = 0; number <=100; number++){
+        
+        if(number %2 === 1){
+            console.log(number, "É impar")
+        }else{
+            console.log(number, "É par")
+        }
+    }
+
+    
+
+}
